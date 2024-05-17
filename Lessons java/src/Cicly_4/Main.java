f94c3907797abfa447c7fc4ec64b62dc08a82cdf
HP <namerandomhp@gmail.com>
2024-05-17 04:42:50 +0300

    Добавлен урок 4 Циклы

---
+++ Lessons java/src/Cicly_4/Main.java
@@ -0,0 +1,7 @@
+package Cicly_4;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- Lessons java/src/UslovnyeOperatory_3/MoneyAmount.java
+++ Lessons java/src/UslovnyeOperatory_3/MoneyAmount.java
@@ -9,11 +9,30 @@ public class MoneyAmount {
         int snickersPrice = 100;
         int bonAquaPrice = 50;
 
+        boolean isCanBuy = false;
+
         System.out.println("Введите кол-во денег: ");
         int moneyAmount = new Scanner(System.in).nextInt();
 
         if (moneyAmount >= twixPrice) {
             System.out.println("Вы можете купить твикс!");
+            isCanBuy = true;
+        }
+        if (moneyAmount >= marsPrice) {
+            System.out.println("Вы можете купить марс!");
+            isCanBuy = true;
+        }
+        if (moneyAmount >= snickersPrice) {
+            System.out.println("Вы можете купить сникерс!");
+            isCanBuy = true;
+        }
+        if (moneyAmount >= bonAquaPrice) {
+            System.out.println("Вы можете купить бонаква!");
+            isCanBuy = true;
+        }
+
+        if (isCanBuy == false) {
+            System.out.println("Увас не хватает денег!");
         }
     }
 }
